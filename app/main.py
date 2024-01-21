a242f387d6c20dc5ec5b106586b08de2c804ccd5
banbao <ilya.kruglove@yandex.ru>
2024-01-21 14:34:02 +0300

    Connect endpoint in main.py: Include the meeting room router for the endpoint to function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.api.meeting_room import router
 from app.core.config import settings
 
 app = FastAPI(
@@ -8,6 +9,8 @@ app = FastAPI(
     description='Room reservation API: An asynchronous application for '
                 'efficient office space booking.',
 )
+
+app.include_router(router)
 
 
 if __name__ == '__main__':
