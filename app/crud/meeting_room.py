b0fa7cbf2a0a0a4a9fc54ded87a806cfa63b2e23
banbao <ilya.kruglove@yandex.ru>
2024-01-21 13:46:45 +0300

    Add Pydantic to ORM conversion in MeetingRoom creation handler

---
+++ app/crud/meeting_room.py
@@ -0,0 +1,16 @@
+from app.core.db import AsyncSessionLocal
+from app.models.meeting_room import MeetingRoom
+from app.schemas.meeting_room import MeetingRoomCreate
+
+
+async def create_meeting_room(
+        new_room: MeetingRoomCreate,
+) -> MeetingRoom:
+    new_room_data = new_room.dict()
+
+    db_room = MeetingRoom(**new_room_data)
+
+    async with AsyncSessionLocal() as session:
+        session.add(db_room)
+
+        await session.commit()
