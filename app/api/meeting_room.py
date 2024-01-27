78bb15bf65a35cc80dd50b5e14c97da0c3d4d7bf
banbao <ilya.kruglove@yandex.ru>
2024-01-27 22:58:18 +0300

    Add 'get_all_meeting_rooms' endpoint to retrieve a list of MeetingRoomDB objects

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -3,7 +3,11 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
-from app.crud.meeting_room import create_meeting_room, get_room_id_by_name
+from app.crud.meeting_room import (
+    create_meeting_room,
+    get_room_id_by_name,
+    read_all_rooms_from_db,
+)
 from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB
 
 router = APIRouter()
@@ -26,3 +30,14 @@ async def create_new_meeting_room(
         )
     new_room = await create_meeting_room(meeting_room, session)
     return new_room
+
+
+@router.get(
+    '/meeting_rooms/',
+    response_model=list[MeetingRoomDB],
+    response_model_exclude_none=True,
+)
+async def get_all_meeting_rooms(
+        session: AsyncSession = Depends(get_async_session),
+):
+    return await read_all_rooms_from_db(session)
