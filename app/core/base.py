f4da18e6ea9c21bc1b58710f1df88f41fa65b981
banbao <ilya.kruglove@yandex.ru>
2024-01-28 20:07:33 +0300

    Add comments how to add new model to DB

--- app/core/base.py
+++ app/core/base.py
@@ -1,4 +1,6 @@
 """Импорты класса Base и всех моделей для Alembic."""
+# alembic revision --autogenerate -m "Add Reservation model"
+# alembic upgrade head
 from app.core.db import Base  # noqa
 from app.models.meeting_room import MeetingRoom  # noqa
 from app.models.reservation import Reservation # noqa
