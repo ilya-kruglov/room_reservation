ee06a591b17adfbdffbe90855c6eaaa731a38a78
banbao <ilya.kruglove@yandex.ru>
2024-02-10 15:21:31 +0300

    Configure FastAPI Users lib: add current_user and current_superuser methods for Dependency Injection, enabling access to the current user during requests and restricting certain endpoints to superusers

--- app/core/user.py
+++ app/core/user.py
@@ -69,3 +69,7 @@ fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
     [auth_backend],
 )
+
+
+current_user = fastapi_users.current_user(active=True)
+current_superuser = fastapi_users.current_user(active=True, superuser=True)
