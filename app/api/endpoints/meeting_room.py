be3cb667c0351c153cfdf5302aa57a91c69e0276
banbao <ilya.kruglove@yandex.ru>
2024-02-10 13:18:44 +0300

    Add retrieving future reservations for a meeting room

--- app/api/endpoints/meeting_room.py
+++ app/api/endpoints/meeting_room.py
@@ -5,11 +5,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.api.validators import check_meeting_room_exists, check_name_duplicate
 from app.core.db import get_async_session
 from app.crud.meeting_room import meeting_room_crud
+from app.crud.reservation import reservation_crud
 from app.schemas.meeting_room import (
     MeetingRoomCreate,
     MeetingRoomDB,
     MeetingRoomUpdate,
 )
+from app.schemas.reservation import ReservationDB
 
 router = APIRouter()
 
@@ -102,3 +104,21 @@ async def get_meeting_room_by_id(
             detail='Meeting Room not found'
         )
     return meeting_room
+
+
+@router.get(
+    '/{meeting_room_id}/reservations',
+    response_model=list[ReservationDB],
+)
+async def get_reservations_for_room(
+        meeting_room_id: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    await check_meeting_room_exists(meeting_room_id, session)
+
+    reservations = await reservation_crud.get_future_reservations_for_room(
+            room_id=meeting_room_id,
+            session=session,
+        )
+
+    return reservations
