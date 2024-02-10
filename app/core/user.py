7cadf5c1636a4d5368fa6c4b45b60a46d9709728
banbao <ilya.kruglove@yandex.ru>
2024-02-10 14:22:17 +0300

    Configure FastAPI Users library: import necessary classes and dependencies

---
+++ app/core/user.py
@@ -0,0 +1,22 @@
+from typing import Optional, Union
+
+from fastapi import Depends, Request
+from fastapi_users import (
+    BaseUserManager,
+    FastAPIUsers,
+    IntegerIDMixin,
+    InvalidPasswordException,
+)
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    BearerTransport,
+    JWTStrategy,
+)
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.config import settings
+from app.core.db import get_async_session
+from app.models.user import User
+from app.schemas.user import UserCreate
+
