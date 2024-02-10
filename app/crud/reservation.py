15604bade746bae21b5a972421391a5725e193b7
banbao <ilya.kruglove@yandex.ru>
2024-02-10 12:20:58 +0300

    Enhance reservation update process, incorporating intersection checks and excluding the current reservation from time conflicts

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from sqlalchemy import and_, select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,21 +12,30 @@ class CRUDReservation(CRUDBase):
 
     async def get_reservations_at_the_same_time(
             self,
+            *,
             from_reserve: datetime,
             to_reserve: datetime,
             meetingroom_id: int,
+            reservation_id: Optional[int] = None,
             session: AsyncSession,
     ) -> list[Reservation]:
-        reservations = await session.execute(
-            select(Reservation).where(
-                Reservation.meetingroom_id == meetingroom_id,
-                and_(
-                    from_reserve <= Reservation.to_reserve,
-                    to_reserve >= Reservation.from_reserve,
-                )
+
+        select_stmt = select(Reservation).where(
+            Reservation.meetingroom_id == meetingroom_id,
+            and_(
+                from_reserve <= Reservation.to_reserve,
+                to_reserve >= Reservation.from_reserve,
             )
         )
-        return reservations.scalars().all()
+
+        if reservation_id is not None:
+            select_stmt = select_stmt.where(
+                Reservation.id != reservation_id
+            )
+
+        reservations = await session.execute(select_stmt)
+        reservations = reservations.scalars().all()
+        return reservations
 
 
 reservation_crud = CRUDReservation(Reservation)
