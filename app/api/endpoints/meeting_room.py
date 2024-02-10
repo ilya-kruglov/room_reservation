59bcddfb32e9b3981787b886e57242cc35c9b6c1
banbao <ilya.kruglove@yandex.ru>
2024-02-10 18:14:38 +0300

    Use 'Depends' in endpoint decorator to restrict access for non-superusers

--- app/api/endpoints/meeting_room.py
+++ app/api/endpoints/meeting_room.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.validators import check_meeting_room_exists, check_name_duplicate
 from app.core.db import get_async_session
+from app.core.user import current_superuser
 from app.crud.meeting_room import meeting_room_crud
 from app.crud.reservation import reservation_crud
 from app.schemas.meeting_room import (
@@ -20,11 +21,13 @@ router = APIRouter()
     '/',
     response_model=MeetingRoomDB,
     response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)],
 )
 async def create_new_meeting_room(
         meeting_room: MeetingRoomCreate,
         session: AsyncSession = Depends(get_async_session),
 ):
+    """Только для суперюзеров."""
     await check_name_duplicate(meeting_room.name, session)
     new_room = await meeting_room_crud.create(meeting_room, session)
     return new_room
@@ -45,12 +48,14 @@ async def get_all_meeting_rooms(
     '/{meeting_room_id}',
     response_model=MeetingRoomDB,
     response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)],
 )
 async def partially_update_meeting_room(
         meeting_room_id: int,
         obj_in: MeetingRoomUpdate,
         session: AsyncSession = Depends(get_async_session),
 ):
+    """Только для суперюзеров."""
     meeting_room = await check_meeting_room_exists(
         meeting_room_id,
         session,
@@ -72,11 +77,13 @@ async def partially_update_meeting_room(
     '/{meeting_room_id}',
     response_model=MeetingRoomDB,
     response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)],
 )
 async def remove_meeting_room(
         meeting_room_id: int,
         session: AsyncSession = Depends(get_async_session),
 ):
+    """Только для суперюзеров."""
     meeting_room = await check_meeting_room_exists(
         meeting_room_id,
         session,
