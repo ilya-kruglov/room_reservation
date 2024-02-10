f32e9d50b804d9ad3efc91ad801697d793b0e4cc
banbao <ilya.kruglove@yandex.ru>
2024-02-10 14:01:02 +0300

    Add User model and import statements for Alembic migrations

--- app/core/base.py
+++ app/core/base.py
@@ -2,5 +2,4 @@
 # alembic revision --autogenerate -m "Add Reservation model"
 # alembic upgrade head
 from app.core.db import Base  # noqa
-from app.models.meeting_room import MeetingRoom  # noqa
-from app.models.reservation import Reservation # noqa
+from app.models import MeetingRoom, Reservation, User  # noqa
