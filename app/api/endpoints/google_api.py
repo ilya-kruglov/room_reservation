699e07b300876d106f8ef895b2a252440b05ef7b
banbao <ilya.kruglove@yandex.ru>
2024-02-28 23:15:30 +0300

    Add business logic for Google API

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -7,8 +7,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.db import get_async_session
 from app.core.google_client import get_service
 from app.core.user import current_superuser
-
 from app.crud.reservation import reservation_crud
+from app.services.google_api import (
+    set_user_permissions,
+    spreadsheets_create,
+    spreadsheets_update_value,
+)
 
 router = APIRouter()
 
@@ -27,5 +31,12 @@ async def get_report(
     """Только для суперюзеров."""
     reservations = await reservation_crud.get_count_res_at_the_same_time(
         from_reserve, to_reserve, session
+    )
+    spreadsheet_id = await spreadsheets_create(wrapper_services)
+    await set_user_permissions(spreadsheet_id, wrapper_services)
+    await spreadsheets_update_value(
+        spreadsheet_id,
+        reservations,
+        wrapper_services,
     )
     return reservations
