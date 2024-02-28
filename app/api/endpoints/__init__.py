4f2884a4d4bcc2444c74bbb407cf46fae80c411e
banbao <ilya.kruglove@yandex.ru>
2024-02-28 22:33:19 +0300

    Add endpoint and func for Google API

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,3 +1,4 @@
+from app.api.endpoints.google_api import router as google_api_router
 from app.api.endpoints.meeting_room import router as meeting_room_router
 from app.api.endpoints.reservation import router as reservation_router
 from app.api.endpoints.user import router as user_router
--- app/api/routers.py
+++ app/api/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import (
+    google_api_router,
     meeting_room_router,
     reservation_router,
     user_router,
@@ -12,5 +13,8 @@ main_router.include_router(
 )
 main_router.include_router(
     reservation_router, prefix='/reservations', tags=['Reservations']
+)
+main_router.include_router(
+    google_api_router, prefix='/google', tags=['Google']
 )
 main_router.include_router(user_router)
